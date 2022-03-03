95d612e89362db8a4bb694f552b9c68d0caf0260
Andrew Cox <drew.r.cox@gmail.com>
2022-03-02 22:34:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
 secretMessage.pop();
-secretMessage.push('to');
-secretMessage.push('Program');
+secretMessage.push('to', 'Program');
 secretMessage[7] = 'right';
 secretMessage.shift();
 secretMessage.unshift('Programming');
